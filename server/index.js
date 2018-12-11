c0726de77ca97809a96cd7c109f2f3e5139e5d7d
John Holler <jh43051@gmail.com>
2018-12-11 15:43:51 -0500

    renamed script.js and added webpack config

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 /* Node Entry Point */
 
-//Declare globals
 const express = require('express') //Add ExpressJS
 const app = express(); //create an Express wrapper
 
 app.listen(8080); //Initiate server
 
-app.use(express.static('client'));  //Serves files from the 'client' dir
+app.use(express.static('client'));  //Serve files from the 'client' dir
